e812680648c14a280cfccb2e70979db617993e33
shewafem <tamirsundureev@gmail.com>
2025-02-25 00:54:35 +0300

    basic functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,25 @@ import React from 'react'
 import { nanoid } from "nanoid"
 
 export default function App() {
-    
+
     const [dice, setDice] = React.useState(generateAllNewDice())
+
+            /**
+     * Challenge: Update the `hold` function to flip
+     * the `isHeld` property on the object in the array
+     * that was clicked, based on the `id` prop passed
+     * into the function.
+     * 
+     * Hint: as usual, there's more than one way to 
+     * accomplish this.
+     */ 
+
+    //const [isDiceClicked, setIsDiceClicked] = React.useState(false)
+    function hold(id) {
+        setDice(oldDice => (oldDice.map(die =>
+            die.id === id ? {...die, isHeld: !die.isHeld} : die))
+        )
+    }
 
     function generateAllNewDice() {
         return new Array(10)
@@ -17,13 +34,28 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(generateAllNewDice())
+        setDice(oldDice =>
+            oldDice.map(die =>
+                die.isHeld ? die : {
+                    ...die,
+                    value: Math.ceil(Math.random() * 6),
+            }
+        ))
     }
 
-    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value}/>)
-    
+    const diceElements = dice.map(dieObj => (
+        <Die 
+        hold={() => {hold(dieObj.id)}} 
+        isHeld={dieObj.isHeld} 
+        key={dieObj.id} 
+        value={dieObj.value}
+        />)
+    )
+
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
